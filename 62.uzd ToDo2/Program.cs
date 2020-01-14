81103c8e2ed63c63e38ca9dbbf8c190bd64dad82
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 17:42:05 +0200

    Add project files.

---
+++ 62.uzd ToDo2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _62.uzd_ToDo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToDo job1 = new ToDo("Buy car", false);
+            ToDo job2 = new ToDo("Sell car", false);
+            ToDo job3 = new ToDo("Buy motorcycle", false);
+
+            Console.WriteLine($"{job1.Name} {job1.Done}");
+
+            // job1.Done = false; // nedarbojas, jo īpašība ir: private set
+
+            job1.MarkAsDone(true);
+
+            Console.WriteLine($"{job1.Name} {job1.Done}");
+        }
+    }
+}
+// Izveidot klasi ToDo ar pēc jūsu domām vajadzīgajiem properties. Obligāti ir jābūt property Done (bool).
+// Izveidot klasē metodi MarkAsDone, kura kā parametru saņem bool tipa mainīgo isDone (tā vērtība var būt true vai false). 
+// Šī metode MarkAsDone ir vienīgā, kura drīkst samainīt properties Done vērtību.
---
+++ 62.uzd ToDo2/ToDo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _62.uzd_ToDo2
+{
+    class ToDo
+    {
+        public string Name { get; set; }
+        public bool Done { get; private set; }
+
+        public ToDo(string workName, bool done)
+        {
+            Name = workName;
+            Done = done;
+        }
+
+        public void MarkAsDone(bool isDone)
+        {
+            Done = isDone;
+        }
+    }
+}
+
+// Izveidot klasi ToDo ar pēc jūsu domām vajadzīgajiem properties. Obligāti ir jābūt property Done (bool).
+// Izveidot klasē metodi MarkAsDone, kura kā parametru saņem bool tipa mainīgo isDone (tā vērtība var būt true vai false). 
+// Šī metode MarkAsDone ir vienīgā, kura drīkst samainīt properties Done vērtību.
